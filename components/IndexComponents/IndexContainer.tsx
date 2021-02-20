14ba6f57ff962300debf6632fa041fd17d344755
Issei <issei.morimoto@gmail.com>
2021-02-20 21:28:33 +0100

    Show song from graphcms

--- components/IndexComponents/IndexContainer.tsx
+++ components/IndexComponents/IndexContainer.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import { SongFragment } from '../../types/types'
 import styles from './Index.module.scss'
+import parse from 'html-react-parser'
 
-export const IndexContainer = () => {
+export const IndexContainer = ({ songs }: { songs: SongFragment[] }) => {
+	const song = songs[0]
 	return (
 		<main>
-			<h1 className={styles.mainTitle}>Pinyin Machine</h1>
-			<iframe
+			{parse(song.youtubeIframe)}
+			<p>{song.lyrics}</p>
+			{/* <iframe
 				width='560'
 				height='315'
 				src='https://www.youtube.com/embed/w_JBF4CJ-9M'
-				frameBorder='0'
+				frameborder='0'
 				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-				allowFullScreen
-			></iframe>
+				allowfullscreen
+			></iframe> */}
 		</main>
 	)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,19 @@
+import { GetStaticProps } from 'next'
 import React from 'react'
 import { IndexContainer } from '../components/IndexComponents/IndexContainer'
+import { graphqlClient } from '../lib/graphql/graphqlClients'
+import { SongFragment } from '../types/types'
 
-export default function Index() {
-	return <IndexContainer />
+export const getStaticProps: GetStaticProps<any> = async () => {
+	const songResult = await graphqlClient.songsQuery()
+	console.log(songResult)
+	return {
+		props: {
+			songs: songResult.songs,
+		},
+	}
+}
+
+export default function Index({ songs }: { songs: SongFragment[] }) {
+	return <IndexContainer songs={songs} />
 }
